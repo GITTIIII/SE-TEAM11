0bca587196d0225cff19ef5fca68ba5acda7332c
dreamkanathip <142082066+dreamkanathip@users.noreply.github.com>
2024-01-08 20:03:25 +0700

    update code

--- frontend/src/page/employee/destination/index.tsx
+++ frontend/src/page/employee/destination/index.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
+// import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+
+import "./destination.css";
+import { Button, ConfigProvider } from "antd";
+import cruise from "../../../asset/cruise.png";
 
 export default function Destination() {
   return (
-    <div>Destination</div>
-  )
+    <div className="cruise-bg" style={{ backgroundImage: `url(${cruise})` }}>
+      <h1 className="destination-header">Destination</h1>
+
+      <div className="destination-headline" />
+
+      <NavLink to="/employee/destination/create">
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: "#CDF5FD",
+              colorTextLightSolid: "#000000",
+              colorPrimaryHover: "#89CFF3",
+              colorPrimaryActive: "#818FB4",
+            },
+          }}
+        >
+          <Button className="destination-add-button" type="primary">
+            add a destination
+          </Button>
+        </ConfigProvider>
+      </NavLink>
+    </div>
+  );
 }
