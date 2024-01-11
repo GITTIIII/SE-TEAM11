13e2701712886e194769c80c716fd827b6f1c879
TitinanJ <titinanjaem@gmail.com>
2024-01-11 12:51:37 +0700

    parn

--- backend/entity/room.go
+++ backend/entity/room.go
@@ -6,19 +6,19 @@ import (
 
 type Room struct {
 	gorm.Model
-	Room_number    	string	`gorm:"uniqueIndex" valid:"required~Issue is required, matches(^[SDAT]\\d{4}$)"`
+	Room_number    	string	`gorm:"uniqueIndex" valid:"required~Issue is required, matches(^[SDAT]\\d{4}$)~Format incorrect"`
 	Room_img		string	`valid:"required~Issue is required"`
 	Status 			string 
 	Room_price 		float64	`valid:"required~Issue is required"`
 
-	RoomTypeID *uint
-	RoomType RoomType `gorm:"foreignKey:RoomTypeID"`
+	RoomTypeID *uint	`valid:"required~Room type is required"`
+	RoomType RoomType	`gorm:"foreignKey:RoomTypeID" valid:"-"`
 
-	RoomZoneID *uint
-	RoomZone RoomZone `gorm:"foreignKey:RoomZoneID"`
+	RoomZoneID *uint	`valid:"required~Room zone is required"`
+	RoomZone RoomZone	`gorm:"foreignKey:RoomZoneID" valid:"-"`
 
 	EmployeeID *uint
-	Employee Employee `gorm:"foreignKey:EmployeeID"`
+	Employee Employee	`gorm:"foreignKey:EmployeeID" valid:"-"`
 
 	Repairs 	[]Repair 	`gorm:"foreignKey:RoomID"`
 	BookPlans 	[]BookPlan 	`gorm:"foreignKey:RoomID"`
