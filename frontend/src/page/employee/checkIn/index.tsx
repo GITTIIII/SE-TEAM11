95ac18e669439d4031cbdeb144a8360ec8133be4
TitinanJ <titinanjaem@gmail.com>
2024-01-15 19:52:45 +0700

    parn

--- frontend/src/page/employee/checkIn/index.tsx
+++ frontend/src/page/employee/checkIn/index.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import cruise from "../../../asset/cruise.png";
+import "./checkIn.css"
 
 export default function CheckIn() {
   return (
-    <div>CheckIn</div>
+    <div className="checkIn-cruise-bg" style={{ backgroundImage: `url(${cruise})` }}>
+      <h1 className='checkIn-header'>Check-In</h1>
+      <div className="checkIn-headline" />
+      <div className='checkIn-form'>
+      <div className='checkIn-text'>Book Plan ID</div>
+        <div className='checkIn-form-control'>
+          <input 
+            className='checkIn-input' 
+            type="text" placeholder = 'Enter book plan ID'
+          />
+          <div className='checkIn-buttom-area'>
+          <button type='submit'>search</button>
+        </div>
+        </div>
+      </div>
+    </div>
+    
   )
 }
